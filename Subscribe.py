02c3c861b79f3498b9b355a59720e3002d547a5f
Kridak <48737384+Kridak@users.noreply.github.com>
2019-06-25 22:50:28 +0530

    Add files via upload

--- Subscribe.py
+++ Subscribe.py
@@ -1,23 +1,145 @@
-#it only will work with the tab 3 in chrome, and when the youtube icon on chrome is positioned at x=733, y=529 (You Need To Change the position as Per Your Requirement)
-import pyautogui as P
-import time
-P.position()
-P.click(x=509, y=13)
-time.sleep(2)
-P.click(x=733, y=529)
-time.sleep(2)
-P.click(x=643, y=102)
-time.sleep(2)
-P.typewrite('Channel Name')
-time.sleep(2)
-P.click(x=1213, y=97)
-time.sleep(3)
-P.click(x=1454, y=259)
-time.sleep(2)
-P.click(x=1507, y=260)
-time.sleep(2)
-P.click(x=710, y=17)
-time.sleep(1)
-P.click(x=473, y=503)
-P.typewrite('Subscribed')
-#Internet Speed Should Be More Than >=2 Megbite/s
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 30,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pyautogui as P\n",
+    "import time\n",
+    "P.position()\n",
+    "P.click(x=509, y=13)\n",
+    "time.sleep(2)\n",
+    "P.click(x=733, y=529)\n",
+    "time.sleep(2)\n",
+    "P.click(x=643, y=102)\n",
+    "time.sleep(2)\n",
+    "P.typewrite('Channel Name')\n",
+    "time.sleep(2)\n",
+    "P.click(x=1213, y=97)\n",
+    "time.sleep(3)\n",
+    "P.click(x=1454, y=259)\n",
+    "time.sleep(2)\n",
+    "P.click(x=1507, y=260)\n",
+    "time.sleep(2)\n",
+    "P.click(x=710, y=17)\n",
+    "time.sleep(1)\n",
+    "P.click(x=473, y=503)\n",
+    "P.typewrite('Subscribed')\n",
+    "#Examples\n",
+    "#x=509, y=13 For New Tab\n",
+    "#x=733, y=529 For Youtube\n",
+    "#x=643, y=102 For Search Bar\n",
+    "#x=1213, y=97 For Search Option\n",
+    "#x=1454, y=259 For Subscribe\n",
+    "#x=1507, y=260 For Bell Icon\n",
+    "#x=710, y=17 For Close Tab\n",
+    "#x=231, y=16 For Close Tab\n",
+    "\n",
+    "#time.sleep(2)\n",
+    "\n",
+    "#P.typewrite\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 25,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "Point(x=487, y=653)"
+      ]
+     },
+     "execution_count": 25,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 27,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "Point(x=473, y=503)"
+      ]
+     },
+     "execution_count": 27,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "raw",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "raw",
+   "metadata": {},
+   "source": [
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
